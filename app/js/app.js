2e5a48f0603a6b63792615e39aee56940a5d5bc8
alex27-ru <medved.lite@gmail.com>
2021-11-11 21:44:26 +0700

    Add help in app.js. Add example import libs in _config.sass. Add mixin button-reset, img-responsive, reset ul, ol, li, p. Add jQ in package.json

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
+/* устанавливать jQuery (с записью в package.json) если не сработало подключение библиотеки - npm install jquery --save */
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
