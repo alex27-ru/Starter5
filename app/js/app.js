7c723d48f3586b41ba41a729be7e2deaf19aa446
alex27-ru <medved.lite@gmail.com>
2021-11-21 17:22:05 +0700

    Add slick slider for feedback. Add lib mixitoup, but not work.

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,33 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
+import '~/app/libs/slick-carousel/slick/slick.min.js'
+import '~/app/libs/mixitup/dist/mixitup.min.js'
 
 /* устанавливать jQuery (с записью в package.json) если не сработало подключение библиотеки - npm install jquery --save */
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
+
+$('.feedback__slider').slick({
+	infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+	nextArrow: '<button type="button" class="slick-next"></button>',
+	prevArrow: '<button type="button" class="slick-prev"></button>'
+
+  });
+
+
+
+
+var mixer = mixitup(containerEl, {
+    selectors: {
+        target: '.feedback__slider'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
 
 })
