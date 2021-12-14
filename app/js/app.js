b6889e59eb9c36a6870260ad786113462e30ada8
alex27-ru <medved.lite@gmail.com>
2021-12-14 23:06:57 +0700

    Add images partners slider. Add partners-slider. Add hover slider items.

--- app/js/app.js
+++ app/js/app.js
@@ -53,7 +53,14 @@ $('.feedback__slider').magnificPopup({
 }
 });
 
+$('.partners__slider').slick({
+	infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+	nextArrow: '<button type="button" class="slick-next"></button>',
+	prevArrow: '<button type="button" class="slick-prev"></button>'
 
+  });
 
 
 })
