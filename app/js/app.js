f0e4e72e8b2315b5f3b05f4c71fdc73c96b28638
Alex27-ru <medved.lite@gmail.com>
2022-02-05 08:41:59 +0700

    add class .sticky for header, .header, logl__img. add js for animate header on scroll

--- app/js/app.js
+++ app/js/app.js
@@ -2,11 +2,17 @@
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
 /* устанавливать jQuery (с записью в package.json) если не сработало подключение библиотеки - npm install jquery --save */
-document.addEventListener('DOMContentLoaded', () => {
-
-$('.burger,.header__burger').click(function(event) {
-		$('.burger,.header__burger').toggleClass('active');
-		$('body').toggleClass('lock');
+document.addEventListener("DOMContentLoaded", () => {
+	$(".burger,.header__burger").click(function (event) {
+		$(".burger,.header__burger").toggleClass("active");
+		$("body").toggleClass("lock");
 	});
 
-})
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$("header,.header,.logo__img").addClass("sticky");
+		} else {
+			$("header,.header,.logo__img").removeClass("sticky");
+		}
+	});
+});
